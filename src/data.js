d698748be414361cb5a23903832bc36ed400889b
brianruhiu <116198613+BrianRuhiuMuhia@users.noreply.github.com>
2022-11-07 09:49:55 +0300

    Update data.js

--- src/data.js
+++ src/data.js
@@ -12,43 +12,43 @@ const data=[
         price:120
     },
     {
-        id:1,
+        id:3,
         name:"Naivasha",
         image:"flamingoes.jpg",
         price:120
     },
     {
-        id:1,
+        id:4,
         name:"Isiolo",
         image:"isiolo.jpg",
         price:120
     },
     {
-        id:1,
+        id:5,
         name:"Kiambu",
         image:"kiambu.jpg",
         price:120
     },
     {
-        id:1,
+        id:6,
         name:"Narok",
         image:"maasai mara.jpg",
         price:120
     },
     {
-        id:1,
+        id:7,
         name:"Logonot",
         image:"Mount-Longonot.jpg",
         price:120
     },
     {
-        id:1,
+        id:8,
         name:"Meru",
         image:"mtKenya.jpg",
         price:120
     },
     {
-        id:1,
+        id:9,
         name:"Thika",
         image:"water fall.jpg",
         price:120
